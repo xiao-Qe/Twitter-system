e1edb39ba7f9d7224bca3012c60e597b73f2dcc2
xiaoQe <2770838499@qq.com>
2023-03-02 16:12:29 +0800

    2023-3-2

---
+++ twitterSystem/src/main/java/com/example/twittersystem/TwitterSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.twittersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterSystemApplication.class, args);
+    }
+
+}

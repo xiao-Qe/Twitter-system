6625e263b18aa88d1f073b83b1c0c1dd78579668
xiaoQe <2770838499@qq.com>
2023-03-13 17:10:26 +0800

    feat: 添加前端vue

---
+++ twitterSystem/src/main/java/com/example/twittersystem/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.twittersystem.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author xiaoQe
+ * @version 1.0
+ * @data 2023/3/11 14:18
+ */
+@Controller
+public class UserController {
+}
---
+++ twitterSystem/src/main/java/com/example/twittersystem/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.twittersystem.service.impl;
+
+import com.example.twittersystem.service.IUserService;
+
+/**
+ * @author xiaoQe
+ * @version 1.0
+ * @data 2023/3/11 14:37
+ */
+public class UserServiceImpl implements IUserService {
+}
